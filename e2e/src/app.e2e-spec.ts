786b325bbb060fa0934db3cb79adf58f56e935db
Dipak Telangre <dipaktelangre@gmail.com>
2020-11-27 12:45:39 +0530

    Add e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,47 @@
-import { AppPage } from './app.po';
+import { TransactionPage } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  let page: TransactionPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new TransactionPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display page title as  "Tezos|Transactions"', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tezos app is running!');
+    expect(page.getTitleText()).toEqual('Tezos|Transactions');
+  });
+
+  it('should display heading as  "Transactions"', () => {
+    page.navigateTo();
+    expect(page.getHeadingText()).toEqual('Transactions');
+  });
+
+  it('should display transactions table row data', () => {
+    page.navigateTo();
+    expect(page.getTransactionsTableFirstRow()).toEqual([
+      '14849585',
+      'transaction',
+      'Sep 6, 2019, 3:45:04 AM',
+      'tz1bDXD6nNSrebqmAnnKKwnX1QdePSMCj4MX',
+      'applied',
+      '0.0035',
+    ]);
+  });
+
+  it('should display 10 transaction count', () => {
+    page.navigateTo();
+    expect(page.getTransactionCount()).toContain(10);
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,30 @@
 import { browser, by, element } from 'protractor';
 
-export class AppPage {
+export class TransactionPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
+  getHeadingText(): Promise<string> {
+    return element(by.css('app-root .title h3')).getText() as Promise<string>;
+  }
+
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return browser.getTitle() as Promise<string>;
+  }
+
+  getTransactionsTableFirstRow(): Promise<Array<string>> {
+    const row = element.all(by.css('table tr.data-row')).first();
+    const cells = row.all(by.tagName('td'));
+    const cellTexts = cells.map(function (elm) {
+      return elm.getText();
+    });
+    return cellTexts as Promise<Array<string>>;
+  }
+
+  getTransactionCount(): Promise<string> {
+    return element(
+      by.css('.transactions-table .transaction-count')
+    ).getText() as Promise<string>;
   }
 }
